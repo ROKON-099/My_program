38e6487b88c9cfb6bc1ae601aaf6334ee1e619c5
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-07-03 22:36:39 +0600

    added a commit

--- 3-7-26/index.js
+++ 3-7-26/index.js
@@ -1,10 +1,16 @@
 
-
-
-
-const title=documnet.getElemenetById('title');
-title.addEventListener('click', function()
-{
-    title.innerText='Clicked';
+const title = document.getElementById('title');
+title.addEventListener('click', function () {
+    title.innerText = 'Clicked';
 })
 
+const btn= document.getElementById('btn');
+btn.addEventListener('click', function() {
+    btn.innerText='Clicked';
+
+})
+
+const btn= document.getElementById('btn');
+btn2.addEventListener('click', function() {
+    btn2.innerText='Clicked';
+});
