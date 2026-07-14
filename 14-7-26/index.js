d62f96aed660030ed1801de9025c71a63408331b
ROKON-099 <mdrokonuzzaman099@gmail.com>
2026-07-14 14:41:22 +0600

    added a commit

--- 14-7-26/index.js
+++ 14-7-26/index.js
@@ -1,1 +1,28 @@
-const count=document
+const count=document.getElementById("count");
+const increase=document.getElementById("increase");
+const decrease=document.getElementById("decrease");
+
+let counter=0;
+increase.addEventListener("click", function(){
+
+counter++;
+count.textContent=counter;
+
+if (counter==10){
+    alert("counter reached maximum !")
+
+increase.style.backgroundColor= "red";
+increase.disabled=true;
+increase.style.cursor = "not-allowed";
+}
+
+
+});
+
+decrease.addEventListener("click", function(){
+
+
+counter--;
+count.textContent=counter;
+
+});
