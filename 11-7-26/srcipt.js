fd27a9548f1e6cbc385338717caeeac1a4639792
ROKON-099 <mdrokonuzzaman099@gmail.com>
2026-07-26 21:44:30 +0600

    add a commit

--- 11-7-26/srcipt.js
+++ 11-7-26/srcipt.js
@@ -1,15 +1,8 @@
- const btn=document.getElementById("btn");
- btn.addEventListener("click", function() {
-
-  btn.textContent="added";
-
-
- });
 
  const btn=document.getElementById("btn");
  btn.addEventListener("click",function(){
     btn.style.backgroundColor="green";
     btn.textContent="purchased";
+    btn.disabled= true;
 
  });
- 
--- 3-7-26/index.js
+++ 3-7-26/index.js
@@ -1,16 +1,1 @@
 
-const title = document.getElementById('title');
-title.addEventListener('click', function () {
-    title.innerText = 'Clicked';
-})
-
-const btn= document.getElementById('btn');
-btn.addEventListener('click', function() {
-    btn.innerText='Clicked';
-
-})
-
-const btn= document.getElementById('btn');
-btn2.addEventListener('click', function() {
-    btn2.innerText='Clicked';
-});
