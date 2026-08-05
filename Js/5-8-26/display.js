b0f02f755877e999ce0071d5aeeae328d7481c49
ROKON-099 <mdrokonuzzaman099@gmail.com>
2026-08-05 18:46:58 +0600

    add display.js

---
+++ Js/5-8-26/display.js
@@ -0,0 +1,8 @@
+const name = "Rokon";
+
+let username = name[0].toLowerCase()+name[1].toLowerCase();
+let username1 = name[0] + name[1] + name [2] + name [3] + name [4] ;
+
+console.log (username);
+console.log (username1);
+
