8e7cdf3ad5f3973234c382edd3d1d4f29c117d90
ROKON-099 <mdrokonuzzaman099@gmail.com>
2026-08-07 20:23:26 +0600

    add navber.jsx

---
+++ Js/7-8-26/navbar.jsx
@@ -0,0 +1,18 @@
+import {Navlink, Link } from "react-router-dom";
+const Navbar = () => {
+
+
+    {
+        name: 'Home', path: '/'
+    },
+    {
+        name: 'About', path: '/about'
+
+    },
+    {
+        name:'contact', path:'/contact'
+
+    },
+    
+
+}
